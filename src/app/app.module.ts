b5f056e7ff98ead5e887d75ef117126a8948fdd7
Liz <adhiamboliz3@gmail.com>
2022-04-09 16:37:33 +0300

    added pipe and directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { HighlightQuoteDirective } from './highlight-quote.directive';
+import { TimeQuotePipe } from './time-quote.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    HighlightQuoteDirective,
+    TimeQuotePipe
   ],
   imports: [
     BrowserModule,
